d3fb10da4eb5c91f3c7336f7d0850b5fa10f7e1e
n-khatsanovskaya <55179752+n-khatsanovskaya@users.noreply.github.com>
2023-06-17 18:41:02 +0200

    Update nodejsdocker.groovy: fixed git username

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('https://github.com/nkhatsanovskaya/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/n-khatsanovskaya/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
